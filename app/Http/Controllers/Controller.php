a36e71103982ba82144cd2b351c7f0ddd5f3fe65
zoe606 <rive99erlangga@gmail.com>
2019-02-15 21:54:47 +0700

    add new function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use \App\Product;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function showAll() 
+    {
+        $dataProductDariModel = Product::all();
+        return view('product.display', ['products' => $dataProductDariModel]);
+    }
+
+    public function saveNew(Request $request)
+    {
+        
+    }
 }
