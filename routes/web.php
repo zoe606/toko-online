ddbca6973bea4f7d8463a9e4fac6b8dad829cb1e
zoe606 <rive99erlangga@gmail.com>
2019-02-18 22:10:38 +0700

    route group

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,26 @@ Route::get('/hellow', function () {
     return 'Hello World';
 });
 
-Route::get('/product/display', 'ProductController@showAll');
-Route::get('/product/save', 'ProductController@saveNew');
-// route param
-Route::get('/product/{id}', 'ProductController@show');
-// route param yang bersifat opsional
-// *Route::get('/product/{id?}', 'ProductController@show');
-
-Route::get('/pintu-masuk', function () {
-    return 'Selamat Datang di pintu masuk!';
+Route::group(['prefix' => '/products'], function () {
+    Route::get('/all', 'ProductController@all');
+    Route::get('/bag', 'ProductController@bag');
+    Route::get('/latest', 'ProductController@latest');
+    Route::get('/discounts', 'ProductController@discounts');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'mustAdmin'], function () {
+    Route::get('/dashboard', 'DashboardController@index');
+    Route::get('/orders', 'DashboardController@orders');
+});
+
+Route::get('/products/display', 'ProductController@showAll');
+Route::get('/products/save', 'ProductController@saveNew');
+// route param
+Route::get('/products/{id}', 'ProductController@show');
+// route param yang bersifat opsional
+// *Route::get('/products/{id?}', 'ProductController@show');
+Route::get('/products/list', 'ProductController@list')->name('products');
+Route::post('/products/{id}', 'ProductController@create');
+Route::match(['PUT', 'PATCH'], '/products/{id}', 'ProductController@update');
 
 Route::get('/users/{user_id}/comments/{comment_id}', 'UserController@showComment');
